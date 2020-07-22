1e2c87128ca1b95e901d2618cd77a762a869dc64
nirkaufman <nirkaufman@gmail.com>
2020-07-21 21:53:30 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <div class="container p-5">
+          <h1>Custom DropDown Component</h1>
+      </div>
+  `,
 })
 export class AppComponent {
-  title = 'interview';
 }
